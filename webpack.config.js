863cd3fd5812bfc1ffebd1fac300835a1b8caf45
Stan Lemon <stanlemon@users.noreply.github.com>
2022-01-11 15:16:51 -0500

    Revert disabling webpack overlay

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,1 @@
-import config from "@stanlemon/webdev/webpack.config.js";
-
-// Temporary as @twilio-paste/core throws a warning
-config.devServer.client = { overlay: false };
-
-export default config;
+export { default } from "@stanlemon/webdev/webpack.config.js";
