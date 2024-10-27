a255fecca2a9f25156cff7acccb0524727232490
Stan Lemon <stanlemon@users.noreply.github.com>
2024-10-26 22:05:13 -0400

    Update dependencies, reverse wonky fix

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,1 @@
-import config from "@stanlemon/webdev/jest.config.js";
-
-export default {
-  ...config,
-  // Something weird is going on that causes the default jest.setup.js to not be imported as a module.
-  // So, for now we skip using that.
-  setupFilesAfterEnv: [],
-};
+export { default } from "@stanlemon/webdev/jest.config.js";
